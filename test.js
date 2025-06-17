08894f2ee9805352670a35c78c29820cb2ed86f9
Rafid <rafidahmed43@gmail.com>
2025-06-17 18:33:41 +0600

    adding js file

---
+++ test.js
@@ -0,0 +1,12 @@
+const accountId =144553
+let accountEmail  = "rafid@gmail.com"
+var accountPassword  = "12345"
+accountCity= "chittagong"
+let accountstate; //it it show undefined in console
+// console.log ("rafid")
+
+accountEmail = "Ahmed@gmail.com"
+accountPassword ="2121"
+accountCity ="dhaka"
+
+console.table ([accountId ,accountEmail,accountPassword,accountCity])
